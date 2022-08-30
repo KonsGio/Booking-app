8261e5f9d2b6ad3d22b5bc10f7ee6b6b1c2989fb
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-30 20:11:30 +0300

    Calendar selecting dates added

--- src/components/navbar/header/Header.jsx
+++ src/components/navbar/header/Header.jsx
@@ -1,11 +1,27 @@
 import { faBed, faCalendarDays, faCar, faMapPin, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './header.css';
+import { DateRange } from 'react-date-range';
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { format } from 'date-fns';
 
 const Header = () => {
+    
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection'
+        }
+    ]);
+
+    const [openDate, setOpenDate] = useState(false);
+
   return (
     <div className='header'>
-        <div className="headerContainer">
+        <div className="headerContainer" >
             <div className="headerList">
                 <div className="headerListItem active">
                     <FontAwesomeIcon icon={faBed} />
@@ -44,7 +60,14 @@ const Header = () => {
                 </div>
                 <div className="headerSearchItem">
                     <FontAwesomeIcon icon={faCalendarDays} className='headerIcon'/>
-                    <span className='headerSearchText'>date to date</span>
+                    <span onClick={() => setOpenDate(!openDate)} className='headerSearchText'>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+                    {openDate && <DateRange
+                        editableDateInputs={true}
+                        onChange={item => setDate([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={date}
+                        className='date'
+                    />}
                 </div>
                 <div className="headerSearchItem">
                     <FontAwesomeIcon icon={faPerson} className='headerIcon'/>
