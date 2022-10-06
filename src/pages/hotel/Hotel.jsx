3068e24f1cb2f2ed518c633c42360536058997c1
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-10-07 00:41:10 +0300

    Bug fix - NaN days

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -24,8 +24,16 @@ const Hotel = () => {
   const {data,loading,error} = useFetch(`/hotels/find/${id}`);
 
   // Bringing data from context api
-  const {dates} = useContext(SearchContext);
-console.log(dates);
+  const {dates,options} = useContext(SearchContext);
+  console.log(dates);
+
+  const MILLISECONDS_PER_DAY = 1000*60*60*24;
+  function dayDifference(date1,date2) {
+    const timeDiff = Math.abs(date2?.getTime() - date1?.getTime());
+    const diffDays = Math.abs(timeDiff / MILLISECONDS_PER_DAY);
+    return diffDays;
+  }
+  const days= (dayDifference(dates[0]?.endDate, dates[0]?.startDate));
 
   const handleOpen = (i) => {
     setSlideNumber(i);
@@ -105,13 +113,13 @@ console.log(dates);
               </p>
             </div>
             <div className="hotelDetailsPrice">
-              <h1>Perfect for a 9-night stay!</h1>
+              <h1>Perfect for a {days}-night stay!</h1>
               <span>
                 Located in the real heart of Krakow, this property has an
                 excellent location score of 9.8!
               </span>
               <h2>
-                <b>$945</b> (9 nights)
+                <b>{days * data.cheapestPrice * options.room}€ </b> ({days} nights)
               </h2>
               <button>Reserve or Book Now!</button>
             </div>
